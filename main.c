014dae8b5e36c1b293fd4b347e3d46371de9f79f
biendltb <biendltb@gmail.com>
2017-07-18 16:28:14 +0800

    lesson10: Recursion drains memory; Stack Overflow error

--- main.c
+++ main.c
@@ -5,16 +5,20 @@
 #define LED_BLUE        (1U << 2)
 #define LED_GREEN       (1U << 3)
 
-unsigned fact(unsigned n);
+int* swap(int* x, int* y);
+int* swap(int* x, int* y)
+{
+    int tmp[2];
+    tmp[0] = *x;
+    tmp[1] = *y;
+    *x = tmp[1];
+    *y = tmp[0];
+    
+    return tmp;
+}
 
 int main()
 {
-  unsigned volatile x;
-  
-  x = fact(0U);
-  x = 2U + 3U * fact(1U);
-  (void)fact(5U);
-  
   SYSCTL_RCGCGPIO_R |= (1U << 5); // enable clock for GPIOF
   SYSCTL_GPIOHBCTL_R |= (1U << 5); // enable AHB for GPIOF
   GPIO_PORTF_AHB_DIR_R = (LED_RED | LED_BLUE | LED_GREEN); // set pin 1, 2, 3 as output
@@ -23,26 +27,26 @@ int main()
   {
       GPIO_PORTF_AHB_DATA_R = LED_BLUE;
       //GPIO_PORTF_AHB_DATA_BITS_R[LED_BLUE] = LED_BLUE;
+      int x = 1000000;
+      int y = 1000000 / 2;
       
-      delay(500000);
+      int *p = swap(&x, &y);
+      
+      delay(p[0]);
       
       GPIO_PORTF_AHB_DATA_BITS_R[LED_RED] = LED_RED;
       
-      delay(500000);
+      p = swap(&x, &y);
+      
+      delay(p[0]);
       
       GPIO_PORTF_AHB_DATA_BITS_R[LED_GREEN] = LED_GREEN;
       
-      delay(500000);
+      p = swap(&x, &y);
+      
+      delay(p[0]);
   }
   
   
   return 0;
-}
-
-unsigned fact(unsigned n)
-{
-  if (n == 0U)
-    return 1U;
-  
-  return n * fact(n - 1);
 }
