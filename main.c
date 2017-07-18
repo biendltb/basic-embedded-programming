53e30ab4f808363ad5c3735446417aab46880231
biendltb <biendltb@gmail.com>
2017-07-18 10:55:46 +0800

    lesson2

--- main.c
+++ main.c
@@ -3,20 +3,19 @@ int main()
 {
   unsigned int counter = 0;
   
-  ++counter;
-  ++counter;
-  ++counter;
-  ++counter;
-  ++counter;
-  ++counter;
-  ++counter;
-  ++counter;
-  ++counter;
-  ++counter;
-  ++counter;
-  ++counter;
-  ++counter;
-  ++counter;
+  while (counter < 21)
+  {
+      ++counter;
+      if ((counter & 1) != 0)
+      {
+        ++counter;
+        /* if counter is odd */
+      }
+      else
+      {
+        /* if counter is even */
+      }
+  }
   
   return 0;
 }
