8f22c54002f0361f2aa9e8075a3969991bff8dcb
biendltb <biendltb@gmail.com>
2017-07-24 14:19:51 +0800

    Lesson20: Race condition

--- main.c
+++ main.c
@@ -1,23 +1,36 @@
 #include <stdint.h>
-#include "tm4c_cmsis.h"
+#include "TM4C123GH6PM.h"
 #include "bsp.h"
 
 int main()
 {
     SYSCTL->RCGC2 |= (1U << 5); // enable clock for GPIOF
-    SYSCTL->GPIOHSCTL |= (1U << 5); // enable AHB for GPIOF
+    SYSCTL->GPIOHBCTL |= (1U << 5); // enable AHB for GPIOF
     
-    GPIOF_HS->DIR = (LED_RED | LED_BLUE | LED_GREEN); // set pin 1, 2, 3 as output
-    GPIOF_HS->DEN = (LED_RED | LED_BLUE | LED_GREEN);
+    GPIOF_AHB->DIR = (LED_RED | LED_BLUE | LED_GREEN); // set pin 1, 2, 3 as output
+    GPIOF_AHB->DEN = (LED_RED | LED_BLUE | LED_GREEN);
     
     SysTick->LOAD       = SYS_CLOCK_HZ / 2U - 1U;
     SysTick->VAL        = 0U;
     SysTick->CTRL       = (1U << 2) | (1U << 1) | 1U;
     
+    //SysTick_Handler();
+    
     __enable_interrupt();
     
     while (1)
     {
+        GPIOF_AHB->DATA_Bits[LED_GREEN] = LED_GREEN;
+        GPIOF_AHB->DATA_Bits[LED_GREEN] = 0U;
+        
+//        __disable_irq();
+//        GPIOF_AHB->DATA |= LED_GREEN;
+//        __enable_irq();
+//        
+//        __disable_irq();
+//        GPIOF_AHB->DATA &= ~LED_GREEN;
+//        __enable_irq();
+        
     }
   
     //return 0;
